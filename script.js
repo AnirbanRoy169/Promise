3630cfacf436378aeaa2b0ab02c9b74dbdd82965
Anirban Roy <156198466+AnirbanRoy169@users.noreply.github.com>
2024-02-10 12:53:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+var typed = new Typed('#element', {
+    strings: ["Promise day ajke", "notun kore Promise korar to nei,", "sob promise to kora e aache.", "Tai na bolo 🌚", "Ro ja promise asbe sob kora hobe.", "Atai ajker Promise", "Decision to decision, akdom.", "Tao ajker reminder...", "Ami tomar Everything", "r", "Tumi amr Watashi No 😘", "Deal Final 😉", "...Ok", "And"],
+    typeSpeed: 50,
+    backDelay: 900,
+    backSpeed: 10,
+});
